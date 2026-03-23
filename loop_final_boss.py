bd9de426c26e75ae678813bd1d7526b6d74a7132
alana sassaki <alanasassaki@gmail.com>
2026-03-23 18:01:53 -0300

    mudanças do mo

--- loop_final_boss.py
+++ loop_final_boss.py
@@ -1,9 +1,15 @@
-arr = [45, 3, 14, 2, 25, 48, 36, 4]
-arr_lenght = len(arr)
+# bubble sort
 
-for i in range(arr_lenght): 
-    for j in range(0, arr_lenght - i - 1): 
-        if arr[j] > arr[j+1]:
-            arr[j], arr[j+1] = arr[j+1], arr[j]
+arr = [45, 3, 14, 2, 25, 48, 36, 4]
+arr_length = len(arr)
+
+def bubble_sort(arr: list[int]):
+    for i in range(arr_length): 
+        swapped = False
+        for j in range(0, arr_length - i - 1): 
+            if arr[j] > arr[j+1]:
+                swapped = True
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+            if not swapped:
+                break
 print(arr)
-        
